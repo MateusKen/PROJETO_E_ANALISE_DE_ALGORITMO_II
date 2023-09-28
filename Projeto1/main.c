945643ee880e768bf8b694d3f14d8306e9744c0b
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-09-28 17:27:48 -0300

    Update main.c

--- Projeto1/main.c
+++ Projeto1/main.c
@@ -3,9 +3,20 @@
 #include <string.h>
 #include <stdbool.h>
 
+void insertion_sort(int *v, int n) {
+  int i, j, aux;
+  for (i = 1; i < n; i++) {
+    aux = v[i];
+    for (j = i - 1; j >= 0 && v[j] > aux; j--) {
+      v[j + 1] = v[j];
+    }
+    v[j + 1] = aux;
+  }
+}
+
 int somaV(int vet[], int n){
   int cont = 0;
-  for(int i = 0; i < n; i++) cont += vet[i];
+  for(int i = 0; vet[i]!='\0'; i++) cont += vet[i];
   return cont;
 }
 
@@ -22,74 +33,67 @@ void printVet(int *vet[], int n){
 
 int lenght(int arr[])
 {
-    int count = 0;
-    for(int i; arr[i]!='\0'; i++)
-    {  
-      count++;
+    int cont = 0;
+    for(int i = 0; arr[i]!='\0'; i++)
+    {
+      cont++;
     }
-    return count;
+    return cont;
 }
 
 
-bool valida(int** vet[], int total, int x, int n){
-  for(int i = 0; i < x; i++){
-    if(somaV(vet[i], n) > (total/x)) return false;
-  }
-  return true;
-}
-
-
-void backup(int** solucao[], int vet[], int n, int x, int total, int row, int col){
-  if((lenght(solucao[0])+lenght(solucao[1])) == n){ // caso base
+void backup(int a[], int b[], int vet[], int n, int total){
+  if((lenght(a)+lenght(b)) == n){ // caso base
     printf("%d GB\n", total);
     printf("Pendrive A (%d GB)\n", total/2);
-    for(int i = 0; i < lenght(solucao[0]); i++)
-      printf("%d GB", solucao[0][i]);
+    for(int i = 0; i < lenght(a); i++)
+      printf("%d GB", a[i]);
     printf("Pendrive B (%d GB)\n", total/2);
-    for(int i = 0; i < lenght(solucao[1]); i++)
-      printf("%d GB", solucao[1][i]);
-    return;
-  }
-
-  for(int i = 0; i < lenght(vet); i++){
-    solucao[row][col] = vet[i];
-    if(valida(solucao[row][col], total, x, n) == true){
-      
-    }
+    for(int i = 0; i < lenght(b); i++)
+      printf("%d GB", b[i]);
   }
   
+  for(int i = 0; i < lenght(vet); i++){
+    a[i] = vet[i];
+    if(somaV(a, n) <= total/2) backup(a, b, vet, n, total); // solução válida
+    else{
+      a[i] = 0;
+      b[i] = vet[i];
+    }
+  }
 }
 
 int main(void) {
   FILE* ptr;
   ptr = fopen("file/backup.txt", "r");
-  if (NULL == ptr) {
-        printf("file can't be opened \n");
+  if (NULL == ptr) 
+  {
+    printf("file can't be opened \n");
     exit(1);
-    }
+  }
 
   int x, total, n, obj;
-  fscanf(ptr, "%d", &x);
+  /*
+  x: é o número de pendrives
+  total: é o total de armazenamento dos dois pendrives
+  n: é o número de arquivos
+  obj: é a variável que guarda o tamanho de cada arquivo
+  */
   
-  int** solucao = (int**)malloc(x * sizeof(int*));
-  
+  fscanf(ptr, "%d", &x); 
   fscanf(ptr, "%d %d", &total, &n);
+
+  int a[n]; // inicia pendrive A
+  int b[n]; // inicia pendrive B
   
   do{
-    int *vet[n]; // vetor que guarda tamanho de cada arquivo
-    for(int i = 0; i < x; i++){ //inicia a matriz de solucoes
-      solucao[i] = (int*)malloc(n*sizeof(int));
-    }
-    
+    int vet[n]; // vetor que guarda tamanho de cada arquivo
     for(int i = 0; i < n; i++){
       fscanf(ptr, "%d", &obj);
-      *vet[i] = obj;
+      vet[i] = obj;
     }
-    printf(vet, n);
-
+    insertion_sort(vet, n); // ordena o vetor dos arquivos
     
 }while(fscanf(ptr, "%d %d", &total, &n) != EOF); // para no fim do arquivo
-  
-  
   return 0;
 }
